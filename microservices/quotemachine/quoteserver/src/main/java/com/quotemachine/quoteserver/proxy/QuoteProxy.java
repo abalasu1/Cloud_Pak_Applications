6dcc88912910cfdfc6ff60673c97f1751da39444
abalasu1 <abalasu1@in.ibm.com>
2020-02-24 16:46:05 +0530

    add quote server

--- microservices/quotemachine/quoteserver/src/main/java/com/quotemachine/quoteserver/proxy/QuoteProxy.java
+++ microservices/quotemachine/quoteserver/src/main/java/com/quotemachine/quoteserver/proxy/QuoteProxy.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="QUOTE-PROXY", url="http://localhost:8001")
+@FeignClient(name="QUOTE-PROXY", url="http://quoteapi:8001")
 public interface QuoteProxy {
-	@GetMapping("/random")
+	@GetMapping("/myquote")
 	String randomquote();
 }
